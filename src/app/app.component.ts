5f6bf2231e20e3cbf26a9f3b16641546a91dade9
Alana <alanac248@gmail.com>
2025-09-08 20:07:16 -0300

    base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, FormsModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component, ViewChild, ElementRef, AfterViewChecked } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms'; // ← Importações adicionais
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent  {
+
+}
